1dec2c98c8f868d6dddf395b75ce76e372c35183
Lu Frazão <lugfrazao@gmail.com>
2024-11-16 14:32:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const repositoriesElement = document.querySelector('#repositories');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+
+    fetch('https://api.github.com/users/lucianafrazao')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            followingElement.innerText = json.following;
+            followersElement.innerText = json.followers;
+            repositoriesElement.innerText = json.public_repos;
+            linkElement.href = json.html_url;
+        })
+})
